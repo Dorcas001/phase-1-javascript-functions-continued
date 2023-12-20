0b185310289ed3184f2aecca31806d90b0bbdead
karimi <karimidorcas1@gmail.com>
2023-12-20 07:20:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun( activity= 'roller-skate' ){
+    return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun()
+function saturdayFun( activity= 'roller-skate' ){
+    return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = 'go to the office' ){
+    return `This Monday, I will ${activity}`
+
+}
+mondayWork()
+
+function mondayWork(activity = 'go to the office' ){
+    return `This Monday, I will ${activity}.`
+
+}
+mondayWork("work from home")
+
+
+function wrapAdjective (activity = '*'){
+    return function(name='special'){
+    return `You are ${activity}${name}${activity}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer")
+
